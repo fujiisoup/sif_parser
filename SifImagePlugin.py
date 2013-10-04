da5cccf281f4e123cb0362f052b93ee9c875380f
Sumant Oemrawsingh <soemraws@xs4all.nl>
2013-10-04 21:20:12 +0200

    Initial try for reading metadata, still incomplete.

--- SifImagePlugin.py
+++ SifImagePlugin.py
@@ -8,6 +8,13 @@ _MAGIC = 'Andor Technology Multi-Channel File\n'
 # --------------------------------------------------------------------
 # SIF reader
 
+def _read_var_len_str(fp):
+    '''Read a string of variable length.  This is encoded by the textual
+    representation of the string length, followed by a newline, followed
+    by the string itself.'''
+    l = int(fp.readline())
+    return fp.read(l)
+
 class SifImageFile(ImageFile.ImageFile):
     format = "SIF"
     format_description = "Andor Technology Multi-Channel File"
@@ -19,9 +26,9 @@ class SifImageFile(ImageFile.ImageFile):
         for i in range(2):
             self.fp.readline()
 
-        self.fp.readline() # Camera
-        self.fp.readline() # Detector dimensions
-        self.fp.readline() # Original filename
+        self.info['Camera'] = self.fp.readline() #var
+        self.info['DetectorDimensions'] = self.fp.readline()
+        self.info['OriginalFilename'] = self.fp.readline()
 
         for i in range(3):
             self.fp.readline()
@@ -31,18 +38,20 @@ class SifImageFile(ImageFile.ImageFile):
         for i in range(15):
             self.fp.readline()
         
-        self.fp.readline() # Frame axis
-        self.fp.readline() # Data type
-        self.fp.readline() # Image axis
+        self.info['FrameAxis'] = self.fp.readline()
+        self.info['XAxis'] = self.fp.readline() #var
+        self.info['ImageAxis'] = self.fp.readline() #var
+	# var that follows is also a data type, probably YAxis
 
-        img_area = self.fp.readline().strip().split()
-        num_frames = 1 + int(img_area[7]) - int(img_area[6])
+        foo = self.fp.readline()
+        img_area = foo.strip().split()
+        num_frames = 1 + int(img_area[7]) - int(img_area[6]) # What's going on here
         frame_area = self.fp.readline().strip().split()
         [x0,x1,y1,y0,ybin,xbin] = map(int,frame_area[1:7])
         width = (1 + x1 - x0) / xbin
         height = (1 + y1 - y0) / ybin
         for frame in range(num_frames):
-            self.fp.read(int(self.fp.readline()))
+            self.fp.read(int(self.fp.readline())) # vars, frame titles?
 
         self.mode = 'F'
         self.size = (width, height)
