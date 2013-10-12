0eeeb82d0a4ade58a0de1de862efcce49c881ab3
Sumant Oemrawsingh <soemraws@xs4all.nl>
2013-10-12 18:18:56 +0200

    Most known metadata is implemented

--- SifImagePlugin.py
+++ SifImagePlugin.py
@@ -20,10 +20,16 @@ def _read_until_space(fp):
     word = ''
     while True:
         c = fp.read(1)
-        if c == ' ':
+        if c == ' ' or c == '\n':
             break
         word += c
     return word
+
+def _read_int(fp):
+    return int(_read_until_space(fp))
+
+def _read_float(fp):
+    return float(_read_until_space(fp))
 
 class SifImageFile(ImageFile.ImageFile):
     format = "SIF"
@@ -34,61 +40,198 @@ class SifImageFile(ImageFile.ImageFile):
             raise SyntaxError('not a SIF file')
 
         # What's this?
-        self.fp.readline()
+        self.fp.readline() # 65538 number_of_images?
 
         # What's this?
-        _read_until_space(self.fp)
-        _read_until_space(self.fp)
-        _read_until_space(self.fp)
-        _read_until_space(self.fp)
-        _read_until_space(self.fp)
+        _read_until_space(self.fp) # 65559
+        _read_until_space(self.fp) # 0
+        _read_until_space(self.fp) # 0
+        _read_until_space(self.fp) # 1
 
-        self.info['DetectorTemperature'] = _read_until_space(self.fp)
-
-        # What is this?
-        _read_string(self.fp, 10)
+        self.info['ExperimentTime'] = _read_int(self.fp)
+        self.info['DetectorTemperature'] = _read_float(self.fp)
 
         # What is this?
-        _read_until_space(self.fp)
-
-        self.info['ExposureTime'] = _read_until_space(self.fp)
+        _read_string(self.fp, 10) # blank
 
         # What is this?
-        self.fp.readline()
+        _read_until_space(self.fp) # 0
 
-        self.info['Camera'] = self.fp.readline() #var
-        self.info['DetectorDimensions'] = self.fp.readline()
-        self.info['OriginalFilename'] = self.fp.readline()
+        self.info['ExposureTime'] = _read_float(self.fp)
+	self.info['CycleTime'] = float(_read_until_space(self.fp))
+	self.info['AccumulatedCycleTime'] = float(_read_until_space(self.fp))
+	self.info['AccumulatedCycles'] = int(_read_until_space(self.fp))
 
-        for i in range(3):
-            self.fp.readline()
+	self.fp.read(1) # NULL
+	self.fp.read(1) # space
 
-        self.fp.readline() # Contains shutter time?
+	self.info['StackCycleTime'] = float(_read_until_space(self.fp))
+	self.info['PixelReadoutTime'] = float(_read_until_space(self.fp))
 
-        for i in range(15):
-            self.fp.readline()
+        # What is this?
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 1
+	self.info['GainDAC'] = _read_float(self.fp)
+
+	# What is this?
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 1e-8
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+
+	# What is this?
+	self.fp.read(1) # 0x04
+	self.fp.read(1) # space
+	self.fp.read(1) # NULL
+	self.fp.read(1) # space
+
+	# What are  these?
+	_read_until_space(self.fp) # 1 or 512
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 4
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0 or 1
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 500000
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # float
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 1 or 4
+	_read_until_space(self.fp) # 0 or 8061
+	_read_until_space(self.fp) # 1
+	_read_until_space(self.fp) # 1
+	_read_until_space(self.fp) # -999
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # -1
+	_read_until_space(self.fp) # 4
+	_read_until_space(self.fp) # 15
+	_read_until_space(self.fp) # 30003
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 1
+
+        self.info['Camera'] = _read_string(self.fp)
+
+	# What is this?
+	self.fp.read(3) # space newline space
+
+	self.info['DetectorDimensions'] = (_read_int(self.fp), _read_int(self.fp))
+
+        self.info['OriginalFilename'] = _read_string(self.fp)
+
+	# What is this?
+	self.fp.read(2) # space newline
+	_read_until_space(self.fp) # 65538
+
+	# What is this?
+	_read_string(self.fp) # Is this some type of experiment comment?
+	self.fp.read(1) # newline
+	_read_int(self.fp) # 65538
+	self.fp.read(8) # 0x01 space 0x02 space 0x03 space 0x00 space
+	self.info['ShutterTime'] = (_read_float(self.fp), _read_float(self.fp)) # ends in newline
+
+	_read_until_space(self.fp) # 65540
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 500
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 1200
+	_read_until_space(self.fp) # 1200 # ends in newline
+
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 10 # ends in newline
+
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	self.fp.read(1) # space
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 10 # ends in newline
+
+	_read_until_space(self.fp) # 0
+	_read_string(self.fp, 6) # SR303i
+	self.fp.read(1) # newline
+
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 10 # ends in newline
+
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 10 # ends in newline
+
+	_read_until_space(self.fp) # 0
+	_read_until_space(self.fp) # 0
+	self.fp.read(1) # newline
+
+	_read_until_space(self.fp) # 65536
+	_read_until_space(self.fp) # 1
+	_read_until_space(self.fp) # 500
+	_read_until_space(self.fp) # 200 # ends in newline
+
+	_read_until_space(self.fp) # 5
+	self.fp.readline() # SR163
+
+        _read_until_space(self.fp) # 65539
+	self.fp.readline() # 0x01 space NULL space 0x01 space NULL space 0x01 space NULL newline
+
+	self.fp.readline() # 0 1 0 0 newline
+	self.fp.readline() # 0 1 0 0 newline
+	self.fp.readline() # 0 1 0 0 newline
+
+	self.fp.readline() # 422 newline or 433 newline
+
+	self.fp.readline() # 13 newline
+	self.fp.readline() # 13 newline
         
-        self.info['FrameAxis'] = self.fp.readline()
-        self.info['XAxis'] = self.fp.readline() #var
-        self.info['ImageAxis'] = self.fp.readline() #var
-        # var that follows is also a data type, probably YAxis
+        self.info['FrameAxis'] = _read_string(self.fp)
+        self.info['DataType'] = _read_string(self.fp)
+        self.info['ImageAxis'] = _read_string(self.fp)
 
-        foo = self.fp.readline()
-        img_area = foo.strip().split()
-        num_frames = 1 + int(img_area[7]) - int(img_area[6]) # What's going on here
+	_read_until_space(self.fp) # 65541
+
+	_read_until_space(self.fp) # 65541
+	_read_until_space(self.fp) # 65541
+	_read_until_space(self.fp) # 65541
+	_read_until_space(self.fp) # 65541
+
+	self.info['NumberOfFrames'] = 1 + _read_int(self.fp) - _read_int(self.fp);
+
+	self.fp.readline() # 1048576 space 1048576 newline
+
+	_read_until_space(self.fp) # 65538
+
+	#foo = self.fp.readline()
+	#print foo
+	#print map(ord, foo)
         frame_area = self.fp.readline().strip().split()
-        [x0,x1,y1,y0,ybin,xbin] = map(int,frame_area[1:7])
+        [x0,x1,y1,y0,ybin,xbin] = map(int,frame_area[:6])
         width = (1 + x1 - x0) / xbin
         height = (1 + y1 - y0) / ybin
-        for frame in range(num_frames):
-            self.fp.read(int(self.fp.readline())) # vars, frame titles?
+        for frame in range(self.info['NumberOfFrames']):
+	     self.info['Frame_{:d}_Comment'] = _read_string(self.fp)
 
         self.mode = 'F'
         self.size = (width, height)
 
         self.tile = []
         offset = self.fp.tell() + 2
-        for frame in range(num_frames):
+        for frame in range(self.info['NumberOfFrames']):
             self.tile.append(('raw', (0,0) + self.size , offset + frame * 4 * width * height, ('F;32F', 0, 1)))
 
 
