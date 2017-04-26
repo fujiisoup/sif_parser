512af636057d733c3b1a6bea3603304b1b02fb19
Keisuke Fujii <fujiisoup@gmail.com>
2017-04-26 17:12:06 +0900

    Start implementing.

--- SifImagePlugin.py
+++ SifImagePlugin.py
@@ -1,3 +1,5 @@
+import numpy as np
+from collections import OrderedDict
 from PIL import Image, ImageFile
 
 # Read Andor Technology Multi-Channel files with PIL.
@@ -7,23 +9,25 @@ _MAGIC = 'Andor Technology Multi-Channel File\n'
 
 # --------------------------------------------------------------------
 # SIF reader
+def _to_string(c):
+    ''' convert bytes to string. c: string or bytes'''
+    return c if not isinstance(c, bytes) else c.decode('utf-8')
 
 def _read_string(fp, length = None):
     '''Read a string of the given length. If no length is provided, the
     length is read from the file.'''
     if length is None:
-        length = int(fp.readline())
+        length = int(_to_string(fp.readline()))
     return fp.read(length)
 
 def _read_until_space(fp):
     '''Read a space-delimited word.'''
     word = ''
     while True:
-        c = fp.read(1)
-        if isinstance(c, bytes):
-            c = c.decode('utf-8')
+        c = _to_string(fp.read(1))
         if c == ' ' or c == '\n':
-            break
+            if len(word) > 0:
+                break
         word += c
     return word
 
@@ -33,22 +37,56 @@ def _read_int(fp):
 def _read_float(fp):
     return float(_read_until_space(fp))
 
+
+class SifImageFile2(ImageFile.ImageFile):
+    format = "SIF"
+    format_description = "Andor Technology Multi-Channel File"
+
+    def _open(self):
+        if _to_string(self.fp.read(36)) != _MAGIC:
+            raise SyntaxError('not a SIF file')
+
+        # 65538 version of andor sif file format
+        self.info['SifVersion'] = _read_until_space(self.fp)
+
+        for p in range(5):
+            is_present = _read_until_space(self.fp)
+            if is_present == '1':
+                self.read_instaimage(p)
+                self.read_calibimage(p)
+                self.read_image_structure(p)
+
+    def read_instaimage(self, p):
+        """ read in the instaimage structure data.
+        p: index of instaimage.
+        """
+        info = OrderedDict()
+        info['version'] = int(_read_until_space(self.fp))
+        info['type'] = int(_read_until_space(self.fp))
+        info['active'] = int(_read_until_space(self.fp))
+        info['structure_version'] = int(_read_until_space(self.fp))
+        info['timedate'] = int(_read_until_space(self.fp))
+        info['temperature'] = float(_read_until_space(self.fp))
+        info['head'] = self.fp.read(2)
+        info['store_type'] = self.fp.read(2)
+        info['data_type'] = _read_until_space(self.fp)
+        info['mode'] = _read_until_space(self.fp)
+        print(info)
+
 class SifImageFile(ImageFile.ImageFile):
     format = "SIF"
     format_description = "Andor Technology Multi-Channel File"
 
     def _open(self):
-        first_line = self.fp.read(36)
-        if isinstance(first_line, bytes):
-            first_line = first_line.decode('utf-8')
-        if first_line != _MAGIC:
+        if _to_string(self.fp.read(36)) != _MAGIC:
             raise SyntaxError('not a SIF file')
 
         # What's this?
         self.fp.readline() # 65538 number_of_images?
 
+        self.info['SifVersion'] = int(_read_until_space(self.fp)) # 65559
+
         # What's this?
-        _read_until_space(self.fp) # 65559
         _read_until_space(self.fp) # 0
         _read_until_space(self.fp) # 0
         _read_until_space(self.fp) # 1
@@ -124,10 +162,13 @@ class SifImageFile(ImageFile.ImageFile):
         _read_until_space(self.fp) # 0
         _read_until_space(self.fp) # 1
 
-        self.info['DetectorType'] = _read_string(self.fp)
-
-        # What is this?
-        self.fp.read(3) # space newline space
+        try:
+            self.info['DetectorType'] = _read_string(self.fp)
+            # What is this?
+            self.fp.read(3) # space newline space
+        except ValueError:
+            # support DV420
+            self.info['DetectorType'] = _to_string(self.fp.readline())
 
         self.info['DetectorDimensions'] = (_read_int(self.fp), _read_int(self.fp))
 
@@ -144,57 +185,30 @@ class SifImageFile(ImageFile.ImageFile):
         self.fp.read(8) # 0x01 space 0x02 space 0x03 space 0x00 space
         self.info['ShutterTime'] = (_read_float(self.fp), _read_float(self.fp)) # ends in newline
 
-        _read_until_space(self.fp) # 65540
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 500
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 1200
-        _read_until_space(self.fp) # 1200 # ends in newline
+        if (65548 <= self.info['SifVersion'] &
+                self.info['SifVersion'] <= 65557):
+            for _ in range(2):
+                self.fp.readline()
+        elif self.info['SifVersion'] == 65558:
+            for _ in range(5):
+                self.fp.readline()
+        elif self.info['SifVersion'] == 65559:
+            for _ in range(9):
+                self.fp.readline()
+        elif self.info['SifVersion'] == 65565:
+            for _ in range(15):
+                self.fp.readline()
+            print(self.fp.readline())
+        elif self.info['SifVersion'] > 65565:
+            for _ in range(18):
+                self.fp.readline()
 
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 10 # ends in newline
+        self.info['SifCalbVersion'] = int(_read_until_space(self.fp)) # 65539
+        # additional skip for this version
+        if self.info['SifCalbVersion'] == 65540:
+            self.fp.readline()
 
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        self.fp.read(1) # space
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 10 # ends in newline
-
-        _read_until_space(self.fp) # 0
-        _read_string(self.fp, 6) # SR303i
-        self.fp.read(1) # newline
-
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 10 # ends in newline
-
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 10 # ends in newline
-
-        _read_until_space(self.fp) # 0
-        _read_until_space(self.fp) # 0
-        self.fp.read(1) # newline
-
-        _read_until_space(self.fp) # 65536
-        _read_until_space(self.fp) # 1
-        _read_until_space(self.fp) # 500
-        _read_until_space(self.fp) # 200 # ends in newline
-
-        _read_until_space(self.fp) # 5
-        self.fp.readline() # SR163
-
-        _read_until_space(self.fp) # 65539
         self.fp.readline() # 0x01 space NULL space 0x01 space NULL space 0x01 space NULL newline
-
         self.fp.readline() # 0 1 0 0 newline
         self.fp.readline() # 0 1 0 0 newline
         self.fp.readline() # 0 1 0 0 newline
@@ -215,18 +229,20 @@ class SifImageFile(ImageFile.ImageFile):
         _read_until_space(self.fp) # y1? right? -> x1
         _read_until_space(self.fp) # y0? top? -> y0
 
-        self.info['NumberOfFrames'] = 1 - _read_int(self.fp) + _read_int(self.fp);
-
-        self.fp.readline() # 1048576 space 1048576 newline
+        no_images = int(_read_until_space(self.fp))
+        no_subimages = int(_read_until_space(self.fp))
+        total_length = int(_read_until_space(self.fp))
+        image_length = int(_read_until_space(self.fp))
+        self.info['NumberOfFrames'] = no_images
 
         _read_until_space(self.fp) # 65538
 
         frame_area = self.fp.readline().strip().split()
-        [x0,x1,y1,y0,ybin,xbin] = map(int,frame_area[:6]) # is order of x and y correct?
+        [x0,x1,y1,y0,xbin,ybin] = map(int,frame_area[:6]) # is order of x and y correct?
         width = int((1 + x1 - x0) / xbin)
         height = int((1 + y1 - y0) / ybin)
-        for frame in range(self.info['NumberOfFrames']):
-             self.info['Frame_{:d}_Comment'] = _read_string(self.fp)
+        #for frame in range(self.info['NumberOfFrames']):
+        #     self.info['Frame_{:d}_Comment'.format(frame)] = _read_string(self.fp)
 
         self.mode = 'F'
         self.size = (int(width), int(height))
@@ -234,8 +250,9 @@ class SifImageFile(ImageFile.ImageFile):
         self.tile = []
         offset = self.fp.tell() + 2
         for frame in range(self.info['NumberOfFrames']):
-            self.tile.append(('raw', (0,0) + self.size , offset + frame * 4 * width * height, ('F;32F', 0, 1)))
-
+            self.tile.append(('raw', (0, 0) + self.size,
+                              offset + frame * 4 * width * height,
+                              ('F;32F', 0, 1)))
 
 # --------------------------------------------------------------------
 # Registry
--- test.py
+++ test.py
@@ -1,12 +1,27 @@
+import numpy as np
 import PIL
 import SifImagePlugin
 import unittest
 
 class test(unittest.TestCase):
     def test_load(self):
-        data = PIL.Image.open('image.sif')
+        data = PIL.Image.open('examples/image.sif')
         data.load()
-        print(data.info)
+        array = np.asarray(data)
+        self.assertTrue(list(array.shape) == [512, 512])
+        print(np.max(array))
+        data.show()
+
+    def test2(self):
+        data = PIL.Image.open('examples/134063.SIF')
+        info = data.info
+        array = np.asarray(data)
+        print(data.tell())
+        print(array.shape)
+        self.assertTrue(list(array.shape) == [1024, 1])
+        data.seek(1)
+        array = np.asarray(data)
+        self.assertTrue(list(array.shape) == [1024, 1])
 
 if __name__ == '__main__':
      unittest.main()
