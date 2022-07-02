bef7906f4c0a877529dac2d77f1fe85dfd6dcc30
Brian Carlsen <carlsen.bri@gmail.com>
2022-07-02 09:48:53 +0200

    [testings/test_cli.sh] Updated folders so script should be run from root directory. Added explicit exit code.

--- testings/test_cli.sh
+++ testings/test_cli.sh
@@ -1,11 +1,14 @@
+# script should be run from root folder
 # test data is stored in public_testdata/cli
 # converted data should be placed in public_testdata/cli/output
 
 # basic usabe
-sif_parser public_testdata/cli/*.sif --output public_testdata/cli/output
+sif_parser ./testings/public_testdata/cli/*.sif --output ./testings/public_testdata/cli/output
 
 # --join flag
-sif_parser public_testdata/cli/*.sif --output public_testdata/cli/output --join
+sif_parser ./testings/public_testdata/cli/*.sif --output ./testings/public_testdata/cli/output --join
 
 # --verbose flag
-sif_parser public_testdata/cli/*.sif --output public_testdata/cli/output --verbose
+sif_parser ./testings/public_testdata/cli/*.sif --output ./testings/public_testdata/cli/output --verbose
+
+exit 0
