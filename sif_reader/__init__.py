76bd156bf8ab93b0b25db8221f2efe4045c965ae
Keisuke Fujii <fUjiisoup@gmail.com>
2022-07-02 08:48:45 -0400

    Updated readme to make the transition clearer (#4)

--- sif_reader/__init__.py
+++ sif_reader/__init__.py
@@ -1,8 +1,4 @@
-raise DeprecationWarning("""
-The `sif_reader` package will be depricated in version 0.4.0.
-Please use the `sif_parser` package instead. The APIs are identical.
-""")
-from ._version import __version__, __version_info__
-from .sif_open import *
+from ..sif_parser._version import __version__, __version_info__
+from ..sif_parser.sif_open import *
 from . import plugin
 from . import utils
