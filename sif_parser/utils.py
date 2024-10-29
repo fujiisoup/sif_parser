112a97e236a117e3a747f693000ae04d0c31ee1f
Keisuke Fujii <fujiisoup@gmail.com>
2024-10-29 12:22:04 -0400

    Using DetectorDimensions by default.

--- sif_parser/utils.py
+++ sif_parser/utils.py
@@ -23,7 +23,9 @@ def extract_calibration(info):
             found.
         None if no calibration is found
     """
-    width = info['ImageLength'] 
+    width = info['DetectorDimensions'][0]
+    if 'ImageLength' in info:
+        width = info['ImageLength'] 
     # multiple calibration data is stored
     if 'Calibration_data_for_frame_1' in info:
         calibration = np.ndarray((info['NumberOfFrames'], width))
