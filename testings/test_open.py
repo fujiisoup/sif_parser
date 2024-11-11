038a1a14d056be4dae5af37e34032abb1e9e2124
Keisuke Fujii <fujiisoup@gmail.com>
2024-11-11 11:31:19 -0500

    Another try for linux

--- testings/test_open.py
+++ testings/test_open.py
@@ -54,6 +54,11 @@ for e in [d + "/encodings/"]:
 def assert_file_not_in_use(filename):
     platform = sys.platform
     if platform == "linux":
+        from subprocess import check_output,Popen, PIPE
+
+        lsout = Popen(['lsof',filename],stdout=PIPE, shell=False)
+        check_output(["grep",filename], stdin=lsout.stdout, shell=False)
+
         for proc in psutil.process_iter():
             try:
                 for item in proc.open_files():
